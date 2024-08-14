dbaac1f17a0815d763a7ce898f4e329f3b09970b
MrT2008 <haitrieu6212@gmail.com>
2024-08-14 22:38:10 +0700

    done  model

--- Images/3_modelImage/script.js
+++ Images/3_modelImage/script.js
@@ -1,20 +1,24 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModel");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+
 img.onclick = function(){
+  // Thay đổi none -> block
   modal.style.display = "block";
+  // đặt link ảnh trong ode
   modalImg.src = this.src;
+  // hiển thị text = alt trong img
   captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
+// nhấn x để đóng
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
+// block -> none
 span.onclick = function() {
   modal.style.display = "none";
 }
